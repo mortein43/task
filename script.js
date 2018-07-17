e5df96498f1aa13929c49972387003133bc7ba04
mortein43 <mortein43@gmail.com>
2018-07-17 22:10:35 +0300

    add mtask2.css

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ btn.addEventListener('click', function(e) {
     e.preventDefault();
     var x = Number(fn.value);
     var y = Number(sn.value);
+    var z = Number(ans.value)
     //alert(x + " " +y );
-    var strUser = slct.options[slct.selectedIndex].text;
-    console.log(x," ",y, " ", strUser);
-    var z = x+y;
-    result.innerHTML = z;
+    var strUser = slct.options[slct.selectedIndex].value;
+    let expected =0;
+    switch(strUser){
+        case "+": expected = Number(x) + Number(y); break;
+        case "-": expected = Number(x) - Number(y); break;
+        case "*": expected = Number(x) * Number(y); break;
+        case "/": expected = Number(x) / Number(y); break;
+    }
+    function showMessage() {
+        if(z === expected) {
+            result.innerHTML = 'Молодець!';
+        } else if(z != expected) {
+            result.innerHTML = 'Спробуй ще.';
+        }
+      }
+      showMessage();
 }
 )
