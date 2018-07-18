abf277e92544fabb10c9ef5d73b7f1df16297e15
mortein43 <mortein43@gmail.com>
2018-07-18 14:11:07 +0300

    add mtask2.css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ btn.addEventListener('click', function(e) {
     e.preventDefault();
     var x = Number(fn.value);
     var y = Number(sn.value);
-    var z = Number(ans.value)
+    var z = Number(ans.value);
     //alert(x + " " +y );
     var strUser = slct.options[slct.selectedIndex].value;
     let expected =0;
@@ -12,6 +12,9 @@ btn.addEventListener('click', function(e) {
         case "*": expected = Number(x) * Number(y); break;
         case "/": expected = Number(x) / Number(y); break;
     }
+    var x = Number(fn.value);
+    var y = Number(sn.value);
+    var z = Number(ans.value);
     function showMessage() {
         if(z === expected) {
             result.innerHTML = 'Молодець!';
