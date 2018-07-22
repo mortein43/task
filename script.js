0854fa197dc64cb94354c5bb267fbdd4d38b0095
mortein43 <mortein43@gmail.com>
2018-07-22 13:03:06 +0300

    add mtask2.css

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ btn.addEventListener('click', function(e) {
     }
 
     if (x==0 || y==0 ){
-        alert("Заповніть поля");
+        result.innerHTML = "Заповніть поля";
+        document.getElementById('result').classList.add('incorrect');
     }
     else{
         showMessage(z, expected);
@@ -25,6 +26,7 @@ btn.addEventListener('click', function(e) {
 function showMessage(z, expected) {
     if(z === expected) {
         result.innerHTML = 'Молодець!';
+        document.getElementById('result').classList.add('correct');
        // var element = document.getElementById("myDIV");
         //element.classList.add("mystyle");
         result.classList.remove('span2');
@@ -33,14 +35,21 @@ function showMessage(z, expected) {
         localStorage["possitive"] = Number(localStorage["possitive"])+1;
         }
          else {
-             localStorage["possitive"] =10;
+             localStorage["possitive"] =1;
          }
         //add class span
         //result2.innerHTML = '';
+
     } else if(z != expected) {
         //result.innerHTML = '';
         result.innerHTML = 'Спробуй ще.';
-        document.getElementById('result').classList.add('span2');
+        document.getElementById('result').classList.add('incorrect');
         //add class span2
+        if (localStorage["negative"]){
+            localStorage["negative"] = Number(localStorage["negative"])+1;
+        }
+        else {
+            localStorage["negative"] =1;
+        }
     }
   }
